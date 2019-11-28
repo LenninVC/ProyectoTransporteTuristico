c379e59c70dd45d6c06beec1e7097022d9add9f1
lennin viciente venancio cordova <leninsc_1993@hotmail.com>
2019-11-27 22:36:46 -0500

    Entidades nuevas y script

---
+++ Transporte/Transporte.Models/Accesos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transporte.Models
+{
+    public class Accesos
+    {
+        public int IdAcceso { get; set; }
+        public string Descripcion { get; set; }
+        public int ParentId { get; set; }
+        public int Level { get; set; }
+        public int Order { get; set; }
+        public string Area { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public string QueryString { get; set; }
+        public string Icon { get; set; }
+        public string Estado { get; set; }
+    }
+}
---
+++ Transporte/Transporte.Models/Permisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transporte.Models
+{
+    public class Permisos
+    {
+        public int IdPermiso { get; set; }
+        public int IdAcceso { get; set; }
+        public int IdPerfil { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
